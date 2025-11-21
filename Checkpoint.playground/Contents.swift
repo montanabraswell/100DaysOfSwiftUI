fa4e32ef753652fb2d7210c8be559a0687c828d6
Montana Braswell <montanabraswell1@gmail.com>
2025-11-21 12:07:32 -0500

    Checkpoint Day 2

---
+++ Checkpoint.playground/Contents.swift
@@ -0,0 +1,13 @@
+// Montana Braswell
+// Checkpoint Question
+// Convert temps from Celsius to Fahrenheit
+
+
+import Cocoa
+
+// create constants for both variables
+let celsius: Double = 25.0
+// convert celsius to fahrenheit
+let fahrenheit: Double =  (9 * celsius / 5) + 32
+// print out results
+print(" \(celsius)", "is equal to \(fahrenheit)")
