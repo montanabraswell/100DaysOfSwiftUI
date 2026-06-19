ab49f5ab529c6f44dbc4ea66deca79125e599f02
Montana Braswell <montanabraswell1@gmail.com>
2026-06-19 13:50:56 -0400

    Finished challenge 3 and fixed a small bug for challenge 2 where the coffee section picker needed to be adjusted

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -24,6 +24,28 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? .now
     }
     
+    var desiredSleepTime: String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            return sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            
+        }
+        return alertMessage
+    }
     var body: some View {
         NavigationStack{
             Form {
@@ -56,24 +78,25 @@ struct ContentView: View {
                     //Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
                     
                     Picker("^[\(coffeeAmount) cup](inflect: true)", selection: $coffeeAmount) {
-                        ForEach(1..<21) {coffee in
+                        ForEach(0..<21) {coffee in
                             Text("\(coffee)")
                         }
                     }
                 }
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                  Text(desiredSleepTime)
+                  
+                }
+               // Section(header: Text("Desired amount of sleep is :")) {
+                    
+                    //create title field
+                    //Text(Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in 4...12, step: 0.25))
+                        //.font(.headline)
             }
             .navigationTitle("BetterRest")
-            .toolbar {
-                //Button("Calculate", action: calculateBedtime)
-                Text("Desired amount of sleep is .. ")
-                    .font(.headline)
-                
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") {}
-            } message: {
-                    Text(alertMessage)
-            }
+          
         }
 
     }
