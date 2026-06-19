0c627cdd59e2120e029645596f139071277a5074
Montana Braswell <montanabraswell1@gmail.com>
2026-06-19 10:56:31 -0400

    New project set up for Project 4 called BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Montana Braswell on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
