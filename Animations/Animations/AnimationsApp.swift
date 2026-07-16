d4389531be5fd64ad28c82bb47b3b261d7567738
Montana Braswell <montanabraswell1@gmail.com>
2026-07-15 23:44:53 -0400

    Finished Day 32 exercise

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Montana Braswell on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
